9706936aa3450b87167d4f5d003d29c41205acdd
Wanko <131977117+WankoNounjio@users.noreply.github.com>
2023-05-29 17:30:32 +0100

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,8 @@
+
+
+
+function Design(){
+    var couleur =prompt("Quelle couleur aimez-vous");
+document.getElementById("java_color").style.backgroundColor= couleur;
+    
+}
---
+++ scrispts.js
@@ -0,0 +1,4 @@
+function Design(){
+    var couleur = prompt("veillez enter votre couleur préférée")
+    document.getElementById("java_color").style.backgroundColor = couleur;
+}
